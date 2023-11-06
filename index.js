42afae99747d206ed5a4b45fbde3e6b14cf80009
Zlr- <mzeler3@gmail.com>
2023-11-06 19:40:22 +0100

    ✨index.js: ajout d'un addeventlistener sur l'input text

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ const nbCoupJoue = document.getElementById("nbCoups")
 const inputReponse = document.getElementById("textResponse");
 const btnSubmit = document.getElementById("btnSubmit");
 
+inputReponse.addEventListener("keyup", () => {
+    let reponseEntree = inputReponse.value.split("");
+    console.log(reponseEntree);
+    reponseEntree.forEach((lettre) => {
+        
+    });
+});
