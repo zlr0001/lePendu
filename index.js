23dfae88ad87123454669c2a891b2f672516afad
Zlr- <mzeler3@gmail.com>
2023-11-06 19:18:19 +0100

    ✨index.html: modification nombre de coups joués

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import {motHasard} from "./modules/motAuHasard.js";
 motHasard();
 
 const imgPendu = document.getElementById("imagePendu");
+const nbCoupJoue = document.getElementById("nbCoups")
 const inputReponse = document.getElementById("textResponse");
 const btnSubmit = document.getElementById("btnSubmit");
 
